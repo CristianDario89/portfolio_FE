fda88c22e3aa328e04cec0e8481017812bc24b44
Cristiano10 <ccristiandarioromero@gmail.com>
2022-05-13 08:48:09 -0300

    3comp + 1serv

--- portfolio_FE/src/app/app.module.ts
+++ portfolio_FE/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercadeComponent } from './componentes/acercade/acercade.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    AcercadeComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
