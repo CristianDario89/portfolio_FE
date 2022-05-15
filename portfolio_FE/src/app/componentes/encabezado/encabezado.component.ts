fda84b7cb48aec7abbf7560bc32fadee21fb5ac5
Cristiano10 <ccristiandarioromero@gmail.com>
2022-05-15 14:24:30 -0300

    implementacion persona OK

--- portfolio_FE/src/app/componentes/encabezado/encabezado.component.ts
+++ portfolio_FE/src/app/componentes/encabezado/encabezado.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { persona } from 'src/app/model/persona.model';
+import { PersonaService } from 'src/app/servicios/persona.service';
 import { PortfolioService } from 'src/app/servicios/portfolio.service';
 @Component({
   selector: 'app-encabezado',
@@ -6,7 +8,23 @@ import { PortfolioService } from 'src/app/servicios/portfolio.service';
   styleUrls: ['./encabezado.component.css']
 })
 export class EncabezadoComponent implements OnInit {
+  persona: persona = new persona("","");
   miPortfolio:any;
+  constructor(public personaService: PersonaService, 
+              private datosPortfolio: PortfolioService) { }
+
+  ngOnInit(): void {
+    this.personaService.obtenerDatos().subscribe(data=>{//metodo q escuche siempre  q el observable haga un cambio
+      console.log("Datos personales"+ JSON.stringify(data));
+      this.persona = data; 
+    });
+
+    this.datosPortfolio.obtenerDatos().subscribe(data =>{
+      console.log(data);
+      this.miPortfolio=data;
+    });
+  }
+/*  miPortfolio:any;
   constructor(private datosPortfolio: PortfolioService) { }
 
   ngOnInit(): void {
@@ -14,6 +32,6 @@ export class EncabezadoComponent implements OnInit {
       console.log(data);
       this.miPortfolio=data;
     });
-  }
+  }*/
 
 }
