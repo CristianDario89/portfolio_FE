71fa4d51f1e6611d9e54efa92fae56bbd1170cf6
digitalessomos <ccristiandarioromero@gmail.com>
2022-07-13 18:06:21 -0300

    nearly4

--- portfolio_FE/src/app/server.ts
+++ portfolio_FE/src/app/server.ts
@@ -1,1 +1,3 @@
-export const URLBack: any = 'http://localhost:8090'
+export const URLBack: any = 'http://localhost:8090';
+
+export const URLBack2: any = 'https://backend-portfolio-romero.herokuapp.com';
