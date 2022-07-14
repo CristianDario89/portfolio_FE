3cc2d108380d229bfbcf28a592485564ccfacdfe
digitalessomos <ccristiandarioromero@gmail.com>
2022-07-14 14:11:04 -0300

    nearly02

--- portfolio_FE/src/app/servicios/login.service.ts
+++ portfolio_FE/src/app/servicios/login.service.ts
@@ -2,12 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../model/user.model';
-import { URLBack2 } from 'src/app/server';
+import { URLBack } from 'src/app/server';
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-  URL: string = `${URLBack2}/api/login/`;
+  URL: string = `${URLBack}/api/login/`;
   constructor(private http: HttpClient) { }
 
   loginUser(user: User): Observable<object> {
